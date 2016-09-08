bbb8a9cdd84d49bbb6a237806310e1cb17baac21
edabot <ed.lewis@gmail.com>
2016-09-08 12:01:56 -0700

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# prism
+# Deck of Cards
+
+The only extra design was to make the Card class as well. It wasn't
+entirely needed, but it was easy to add. It would be handy to have if
+there was a need for different display options for the card info such as
+"AH" or "Ace of Hearts."
+
+I used the Fisher-Yates algorithm for shuffling the cards. It's O(n),
+which is always nice.
+
+If I wanted to be more thorough, I could add testing to make sure that
+all of the cards are different after shuffling. That could be done with
+a 52-length array or a hash.
+
+#Prism staff
+
+Run with "ruby staff.rb"
+
+I tried a few different ways to do this with JavaScript or jQuery, but
+kept having cross-domain issues. Finally looked into Nokogiri and found
+it perfectly suited for the task.
+
+The only design decision here was to add a ew line breaks to make the
+results a bit easier to read.
--- deck_of_cards.js
+++ deck_of_cards.js
@@ -1,26 +1,8 @@
-// Deck of Cards
-// -------------
-//
-// Create a framework that models a generic deck of cards, with the following
-// behaviors:
-//
-//   * Should contain an operation named Shuffle() that shuffles this deck
-//     randomly.
-//   * Should contain an operation called GetNextCard() that returns the next
-//     card from the deck
-//   * GetNextCard() should signal an error when the entire deck has been dealt
-//     out
-//   * A call to Shuffle() should cause all cards to be put back in the deck
-//
-// Using this framework, create a class to model a standard deck of 52 playing
-// cards that contain 4 suits of 13 cards each.
-//
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 
-var suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-
-var Card = function(suit, val) {
+var Card = function(suit, value) {
   this.suit = suit;
-  this.val = val;
+  this.value = value;
 }
 
 var Deck = function() {
